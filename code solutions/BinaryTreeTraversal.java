f1b370e4fb347363bdd4c0ce92e1068fee85b2a5
Niththish <44729023+niththish@users.noreply.github.com>
2021-09-22 20:22:44 +0530

    Update BinaryTreeTraversal.java
    included postorder traversal

--- code solutions/BinaryTreeTraversal.java
+++ code solutions/BinaryTreeTraversal.java
@@ -61,4 +61,27 @@ public class BinaryTreeTraversal {
         return result;
     }
     /*-----------------------End of preorder Tracersal----------------------------------*/
+      
+      
+    /*-----------------------Postorder Tracersal----------------------------------*/
+    List<Integer> res  = new ArrayList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root == null){
+            return res;
+        }
+
+        if(root.left != null){
+            postorderTraversal(root.left);
+        }
+
+        if(root.right != null){
+            postorderTraversal(root.right);
+        }
+
+        res.add(root.val);
+
+        return res;
+    }
+    /*-----------------------End of Postorder Tracersal----------------------------------*/
 }
